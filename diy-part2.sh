470b0276a1094c87d5856f8dd65242bea458de8e
Ray <railzy00@gmail.com>
2021-11-02 11:00:05 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-#============================================================
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#============================================================
-
-#安装华工路由器插件
-git clone https://github.com/scutclient/scutclient.git
-cp -R ./scutclient/openwrt ./package/scutclient
-git clone https://github.com/scutclient/luci-app-scutclient.git ./feeds/luci/applications/luci-app-scutclient
-./scripts/feeds install -a -p luci
+#
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
