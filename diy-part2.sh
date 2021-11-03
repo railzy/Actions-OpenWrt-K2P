048fbf9e9a43d5d49d472d26d4383c149faa8008
Ray <railzy00@gmail.com>
2021-11-03 19:49:57 +0800

    Modify default theme

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
+# Modify default ntp server
 sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
