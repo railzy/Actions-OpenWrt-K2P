afee8e72031a7a608f0096cd1959919d10987ba3
railzy <57893649+railzy@users.noreply.github.com>
2023-02-12 02:06:37 +0800

    argon 最新版

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,3 +23,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # 添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+
+# argon 最新版，参考 https://github.com/jerrykuku/luci-theme-argon/issues/297
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-18.06
+git clone https://github.com/jerrykuku/luci-theme-argon-config.git package/luci-theme-argon-config
